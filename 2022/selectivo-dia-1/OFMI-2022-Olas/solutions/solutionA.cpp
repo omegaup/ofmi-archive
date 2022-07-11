9d5179a0d37564cf961ea4049cbcc3d984e6acbe
DiegoBriaares <DiegoBriaares@users.noreply.github.com>
2022-07-11 03:30:39 +0000

    Fixed /hook_tools/lint.py lints

--- 2022/selectivo-dia-1/OFMI-2022-Olas/solutions/solutionA.cpp
+++ 2022/selectivo-dia-1/OFMI-2022-Olas/solutions/solutionA.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
+
 #include "waves.h"
 
 const int INF = 1e9;
@@ -11,27 +12,28 @@ const int MAX = 101;
 int memo[MAX][MAX][MAX];
 
 int dp(int i, int l, int k) {
-	if (k > K) return INF;
-	if (i > N + 1) {
-		return std::max(0, i - l - 1);
-	}
-	if (memo[i][l][k] != -1) {
-		return memo[i][l][k];
-	}
-	memo[i][l][k] = INF;
-	memo[i][l][k] = std::min(memo[i][l][k], dp(i + 1, l, k));
-	memo[i][l][k] = std::min(memo[i][l][k], dp(i + 1, i, k + i - l - 1) + (int)(a[l] > a[i]));
-	return memo[i][l][k];
+  if (k > K) return INF;
+  if (i > N + 1) {
+    return std::max(0, i - l - 1);
+  }
+  if (memo[i][l][k] != -1) {
+    return memo[i][l][k];
+  }
+  memo[i][l][k] = INF;
+  memo[i][l][k] = std::min(memo[i][l][k], dp(i + 1, l, k));
+  memo[i][l][k] =
+      std::min(memo[i][l][k], dp(i + 1, i, k + i - l - 1) + (int)(a[l] > a[i]));
+  return memo[i][l][k];
 }
 
-	int waves (int n, int k, int A[]) {
-		N = n;
-		K = k;
-		for (int i = 0; i < n; i++) {
-			a[i + 1] = A[i];
-		}
-		memset(memo, -1, sizeof(memo));
-		a[0] = -INF;
-		a[n + 1] = INF;
-		return dp(1, 0, 0);
-	}
+int waves(int n, int k, int A[]) {
+  N = n;
+  K = k;
+  for (int i = 0; i < n; i++) {
+    a[i + 1] = A[i];
+  }
+  memset(memo, -1, sizeof(memo));
+  a[0] = -INF;
+  a[n + 1] = INF;
+  return dp(1, 0, 0);
+}
